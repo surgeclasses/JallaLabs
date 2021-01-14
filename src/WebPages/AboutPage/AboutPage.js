c281585da7eb170cec0df7c04b837012644fc203
Yash <surge123@surgeclasses.com>
2021-01-14 14:55:25 +0530

    yash changes

--- src/WebPages/AboutPage/AboutPage.js
+++ src/WebPages/AboutPage/AboutPage.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./AboutPage.css";
+// import { RiLiveFill } from "react-icons/ri";
+// import { HiUserGroup } from "react-icons/hi";
+// import { FaProjectDiagram } from "react-icons/fa";
+// import { ImOffice } from "react-icons/im";
 
 const AboutPage = () => {
   return (
     <div>
-       <h1> About Page </h1>
-       <h2> Development started by Raman </h2>
     </div>
+    // <div className="features">
+    //         <ul>
+    //             <li className="feature-box1">
+    //             <RiLiveFill className="feature-icons" />
+    //             <h1 className="feature-h1Fonts">Live Classes</h1>
+    //             <p className="feature-pFonts">• Online classes taught through video have already proven their effectiveness and preference for the vast majority of viewers.
+    //             Through recorded classes the student is able to see and hear the teacher, facilitating the dynamics and connection that occurs in a classroom. •</p>
+    //             </li>
+
+    //             <li className="feature-box2">
+    //             <HiUserGroup className="feature-icons" />
+    //             <h1 className="feature-h1Fonts">Expert Instructors</h1>
+    //             <p className="feature-pFonts">• Even with all the right information available, human involvement cannot be omitted. With our mentor support team, 
+    //             Surge Classes offers an option to get your doubts cleared by experts and interact with a personal online instructor for any doubts you may have. •</p>
+    //             </li>
+                
+    //             <li className="feature-box3">
+    //             <FaProjectDiagram className="feature-icons" />
+    //             <h1 className="feature-h1Fonts">Multiple Projects</h1>
+    //             <p className="feature-pFonts">• Managing multiple projects presents different and additional challenges when compared with the management of a single project. 
+    //             This acquired knowledge and skills in the areas of scope, time, cost, risk, human resources,
+    //              leadership, and teambuilding. •</p>
+    //             </li>
+                
+    //             <li className="feature-box4">
+    //             <ImOffice className="feature-icons" />
+    //             <h1 className="feature-h1Fonts">Industry Oriented</h1>
+    //             <p className="feature-pFonts">• Industry oriented education is an approach to learning from an industry perspective. With traditional technical teaching methodologies in educational environments,
+    //              the conventional pathway is to build the foundation learning through real projects enviornment. •</p>
+    //             </li>
+    //         </ul>
+    //     </div>
   );
 };
 
--- src/WebPages/ContactPage/ContactPage.js
+++ src/WebPages/ContactPage/ContactPage.js
@@ -1,12 +1,67 @@
 import React from "react";
+import { ImPhone, ImLocation, ImMail2 } from "react-icons/im";
 import "./ContactPage.css";
 
 const ContactPage = () => {
   return (
-    <div>
-       <h1> Contact Page </h1>
-       <h2> Development started by Raman </h2>
+    
+    <div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-left">
+          <div class="screen-header-button close"></div>
+          <div class="screen-header-button maximize"></div>
+          <div class="screen-header-button minimize"></div>
+        </div>
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <div class="app-title">
+            <span>CONTACT</span>
+            <span>US</span>
+          </div>
+          <div class="app-contact">CONTACT INFO
+          </div>
+          <div class="app-contact-info">
+          <p><ImLocation style={{marginRight: "0.5rem", blockSize: "12px"}}/>No. 15, 25Th A Main, Mariswamy Layout
+          Agara, HSR Layout, Sector - 1
+          Bengaluru</p>
+          <p><ImPhone style={{marginRight: "0.5rem", blockSize: "12px"}}/>+91 9014089189</p>
+          <p><ImMail2 style={{marginRight: "0.5rem", blockSize: "12px"}}/>surgeclasses2020@gmail.com</p>
+          </div>
+          </div>
+        <div class="screen-body-item">
+          <div class="app-form">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME"></input>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL"></input>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="CONTACT NO"></input>
+            </div>
+            <div class="app-form-group message">
+              <input class="app-form-control" placeholder="MESSAGE"></input>
+            </div>
+            <div class="app-form-group buttons">
+            <button class="app-form-button" onClick={()=>console.log("Success")}>SEND</button>
+              <button class="app-form-button">CANCEL</button>
+             
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
